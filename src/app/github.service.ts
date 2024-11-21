ccb7397bb6d0535938f7aac230e5eb4fbea51a07
chirayu16 <chirayupo16@gmail.com>
2024-11-21 23:01:06 +0530

    pagination fix

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -3,23 +3,18 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GithubService {
   private baseUrl: string = 'https://api.github.com/users';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   searchUser(userName: string): Observable<any> {
-
     return this.http.get<any>(`${this.baseUrl}/${userName}`);
   }
 
-  getUserRepos(
-    username: string,
-  ): Observable<any> {
-    return this.http.get<any>(`${this.baseUrl}/${username}/repos`);
+  getUserRepos(username: string, page:number, perPage: number): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/${username}/repos?page=${page}&per_page=${perPage}`);
   }
-  
-
 }
